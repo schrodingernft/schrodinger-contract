dde5fb6dd9a1e5308478d873a77706154f77e34f
suerlych <suer@schrodingernft.ai>
2024-03-01 21:19:27 +0800

    fix

--- contract/Schrodinger/SchrodingerContract_Actions.cs
+++ contract/Schrodinger/SchrodingerContract_Actions.cs
@@ -24,6 +24,7 @@ public partial class SchrodingerContract : SchrodingerContractContainer.Schrodin
         AssertAdmin();
         Assert(input != null && !input.Value.IsNullOrEmpty(), "Invalid input.");
         State.Admin.Value = input;
+        
         return new Empty();
     }
 
