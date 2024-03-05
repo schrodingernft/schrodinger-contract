d97ff68684ec92fab08bc1b5f8ba14a43e0d29af
suerlych <suer@schrodingernft.ai>
2024-03-05 23:22:03 +0800

    add ut

--- test/Schrodinger.Contracts.Tests/SchrodingerContractTests_Admin.cs
+++ test/Schrodinger.Contracts.Tests/SchrodingerContractTests_Admin.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using AElf;
 using AElf.Types;
 using Google.Protobuf.WellKnownTypes;
 using Shouldly;
@@ -63,5 +64,53 @@ public partial class SchrodingerContractTests : SchrodingerContractTestBase
         await SchrodingerContractUserStub.SetAdmin.SendAsync(DefaultAddress);
         admin = await SchrodingerContractStub.GetAdmin.CallAsync(new Empty());
         admin.ShouldBe(DefaultAddress);
+    }
+    
+    [Fact]
+    public async Task InitializeTests_All()
+    {
+        //initialize not deployer
+        var result =  await SchrodingerContractUser2Stub.Initialize.SendWithExceptionAsync(new InitializeInput
+        {
+            Admin = DefaultAddress,
+            PointsContractDappId = new Hash(),
+            PointsContract = DefaultAddress
+        });
+        result.TransactionResult.Error.ShouldContain("No permission");
+        
+        // initialize is deployer
+        var init = new InitializeInput
+        {
+            Admin = DefaultAddress,
+            PointsContractDappId = new Hash(),
+            PointsContract = DefaultAddress
+        };
+        await SchrodingerContractStub.Initialize.SendAsync(init);
+        var admin = await SchrodingerContractStub.GetAdmin.CallAsync(new Empty());
+        admin.ShouldBe(DefaultAddress);
+        var PointsContractDappId = await SchrodingerContractStub.GetPointsContractDAppId.CallAsync(new Empty());
+        PointsContractDappId.ShouldNotBe(null);
+        var PointsContract = await SchrodingerContractStub.GetPointsContract.CallAsync(new Empty());
+        PointsContract.Value.ShouldBe(DefaultAddress.Value);
+        
+        // initialize twice
+        result = await SchrodingerContractStub.Initialize.SendWithExceptionAsync(new InitializeInput
+        {
+        });
+        result.TransactionResult.Error.ShouldContain("Already initialized.");
+        
+        //set PointsContract
+        var resultPointsContract =  await SchrodingerContractUser2Stub.SetPointsContract.SendWithExceptionAsync(DefaultAddress);
+        resultPointsContract.TransactionResult.Error.ShouldContain("No permission");
+        
+        resultPointsContract =  await SchrodingerContractStub.SetPointsContract.SendWithExceptionAsync(DefaultAddress);
+        resultPointsContract.TransactionResult.Error.ShouldContain("has Configured");
+        
+        //set PointsContractDappId
+        var resultPointsContractDappId =  await SchrodingerContractUser2Stub.SetPointsContractDAppId.SendWithExceptionAsync(new Hash());
+        resultPointsContractDappId.TransactionResult.Error.ShouldContain("No permission");
+        
+        resultPointsContractDappId =  await SchrodingerContractStub.SetPointsContractDAppId.SendWithExceptionAsync(HashHelper.ComputeFrom("points"));
+        resultPointsContractDappId.TransactionResult.Error.ShouldContain("has Configured");
     }
 }
